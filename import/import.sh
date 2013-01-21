28cea3b546780b41196c25e140cabc0e2074734c
Andrey <andrey.kalinovsky@gmail.com>
2013-01-21 12:02:55 +0100

    Fix import script

--- import/import.sh
+++ import/import.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+
+host=""
+user=""
+password=""
+database="geonames"
+collection="countries"
+elastichost="http://127.0.0.1:9200/"
+
+while getopts ":h:u:p:d:c:e:" opt; do
+  case $opt in
+    h)
+      host="--host $OPTARG"
+      ;;
+    u)
+      user="--user $OPTARG"
+      ;;
+    p)
+      password="--password $OPTARG"
+      ;;
+    d)
+      database="$OPTARG"
+      ;;
+    c)
+      collection="$OPTARG"
+      ;;
+    e)
+      elastichost="$OPTARG"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG \n\nUsage: sh import.sh\n\t[-h hostname]\n\t[-u user]\n\t[-p password]\n\t[-d database]\n\t[-c collection]\n\t[-e elasticsearchhostname]\n" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
 MONGO_VERSION=`mongo --version | sed -e "s|.*: \.*||" | cut -d "." -f -1`
 
 if [ -z `which mongo` ]; then
@@ -21,20 +60,20 @@ wget http://download.geonames.org/export/dump/allCountries.zip
 unzip allCountries.zip
 rm allCountries.zip
 
-mongo geonames dropDB.js
+mongo $host $user $password $database dropDB.js
 
 if [ $MONGO_VERSION -gt 1 ]; then
-    mongoimport -d geonames -c countries --type tsv \
-	--fields geonameid,name,asciiname,alternatenames,latitude,longitude,featureClass,featureCode,countryCode,cc2,admin1Code,admin2Code,admin3Code,admin4Code,population,elevation,DEM,timezone,modificationDate \
-	--stopOnError allCountries.txt
+    mongoimport $host $user $password -d $database -c $collection --type tsv \
+        --fields geonameid,name,asciiname,alternatenames,latitude,longitude,featureClass,featureCode,countryCode,cc2,admin1Code,admin2Code,admin3Code,admin4Code,population,elevation,DEM,timezone,modificationDate \
+        --stopOnError allCountries.txt
 else
-    mongoimport -d geonames -c countries --type tsv \
-	--fields geonameid,name,asciiname,alternatenames,latitude,longitude,featureClass,featureCode,countryCode,cc2,admin1Code,admin2Code,admin3Code,admin4Code,population,elevation,DEM,timezone,modificationDate \
-	 allCountries.txt
+    mongoimport $host $user $password -d $database -c $collection --type tsv \
+        --fields geonameid,name,asciiname,alternatenames,latitude,longitude,featureClass,featureCode,countryCode,cc2,admin1Code,admin2Code,admin3Code,admin4Code,population,elevation,DEM,timezone,modificationDate \
+         --stopOnError allCountries.txt
 fi
 
 rm allCountries.txt
-mongo geonames setupDB.js
+mongo $host $user $password $database setupDB.js
 
 rm countryInfo.txt admin1CodesASCII.txt admincodes.txt countrynames.txt
 wget http://download.geonames.org/export/dump/admin1CodesASCII.txt
@@ -44,9 +83,9 @@ wget http://download.geonames.org/export/dump/countryInfo.txt
 cat countryInfo.txt | grep -v '^#' | cut -f1,5 > countrynames.txt
 rm countryInfo.txt
 
-mongoimport -d geonames -c admincodes --type tsv --fields code,name --stopOnError admincodes.txt
-mongoimport -d geonames -c countrynames --type tsv --fields code,name --stopOnError countrynames.txt
+mongoimport $host $user $password -d $database -c admincodes --type tsv --fields code,name --stopOnError admincodes.txt
+mongoimport $host $user $password -d $database -c countrynames --type tsv --fields code,name --stopOnError countrynames.txt
 
 rm countrynames.txt admincodes.txt
 
-php ./index.php $1 geonames countries
+php ./index.php $elastichost $database $collection $host $user $password
--- import/index.php
+++ import/index.php
@@ -1,32 +1,28 @@
 <?php
 
-if (isset($argv[1]) && !empty($argv[1]))
-  $varElasticSearchUrl = $argv[1];
-if (isset($argv[2]) && !empty($argv[2]))
-  $varMongoDBName = $argv[2];
-if (isset($argv[3]) && !empty($argv[3]))
-  $varMongoCollectionName = $argv[3];
-	  
+include __DIR__ . "/vars.php";
 
-require_once __DIR__ . '/vendor/autoload.php';
+$varMongoUsername = "";
+$varMongoPassword = "";
+$varMongoHost = "localhost";
 
-use \ElasticSearch\Client;
-
-include("vars.php");
-
-function getElasticSearchUrl($url, $db, $collection,
-			     $mongoDb, $mongoCollection) {
-  if (substr($url, -1) != "/")
-    $url .= "/";
-  if (isset($db) && !empty($db))
-    $url .= $db . "/";
-  else
-    $url .= $mongoDb . "/";
-  if (isset($collection) && !empty($collection))
-    $url .= $collection . "/";
-  else
-    $url .= $mongoCollection . "/";
-  return ($url);
+if (isset($argv[1]) && !empty($argv[1])) {
+    $varElasticSearchUrl = $argv[1];
+}
+if (isset($argv[2]) && !empty($argv[2])) {
+    $varMongoDBName = $argv[2];
+}
+if (isset($argv[3]) && !empty($argv[3])) {
+    $varMongoCollectionName = $argv[3];
+}
+if (isset($argv[4]) && !empty($argv[4])) {
+    $varMongoHost = $argv[4];
+}
+if (isset($argv[5]) && !empty($argv[5])) {
+    $varMongoUserName = $argv[5];
+}
+if (isset($argv[6]) && !empty($argv[6])) {
+    $varMongoPassword = $argv[6];
 }
 
 function getUrl($url, $db, $mongoDb) {
@@ -70,10 +66,25 @@ try
   $m = new Mongo();
   $db = $m->$varMongoDbName;
 }
-catch (MongoConnectionException $e)
-{
-  echo 'Couldn\'t connect to mongodb, is the "mongo" process running?';
-  exit();
+
+try {
+  var_dump($varMongoHost);
+  var_dump($varMongoUsername);
+  var_dump($varMongoPassword);
+  if (!empty($varMongoUsername) && !empty($varMongoPassword))
+    $m = new MongoClient("mongodb://$varMongoHost",
+                         array("username" => $varMongoUsername,
+                               "password" => $varMongoPassword));
+  else if (!empty($varMongoUsername))
+    $m = new MongoClient("mongodb://$varMongoHost",
+                         array("username" => $varMongoUsername));
+  else
+    $m = new MongoClient("mongodb://$varMongoHost");
+  $db = $m->$varMongoDbName;
+    
+} catch (MongoConnectionException $e) {
+    echo "Couldn't connect to mongodb, is the 'mongo' process running?\n";
+    exit();
 }
 
 echo "Connected to the database $varMongoDbName.\n";
@@ -95,67 +106,54 @@ $rootUrl = getUrl($varElasticSearchUrl,
 
 
 
-$es = Client::connection($URL);
-if (isset($es))
-  {
+echo "Dropping current ElasticSearch index.\n";
+exec("sh ./scripts/deleteIndex.sh $rootUrl");
+echo "Creating new index.\n";
+exec("sh ./scripts/createIndex.sh $rootUrl");
+echo "Setting geolocation parameters.\n";
+exec("sh ./scripts/setGeolocation.sh $URL" . "_mapping $varMongoCollectionName");
 
-    echo "Dropping current ElasticSearch index.\n";
-    exec("sh ./scripts/deleteIndex.sh $rootUrl" );
-    echo "Creating new index.\n";
-    exec("sh ./scripts/createIndex.sh $rootUrl");
-    echo "Setting geolocation parameters.\n";
-    exec("sh ./scripts/setGeolocation.sh $URL"."_mapping $varMongoCollectionName");
+echo "\nIndexing...\n";
 
-    echo "\nIndexing...\n";
+$conn = curl_init();
+$flag = 0;
 
-    $conn = curl_init();
-    $flag = 0;
-
-    while ($flag == 0)
-      {
-	try {
-	  foreach ($cursor as $obj)
-	    {
-	      $i++;
-	      $mongoid = $obj['_id'];	
-	      unset($obj['_id']);
-	      curl_setopt($conn, CURLOPT_URL, $URL . $i);
-	      curl_setopt($conn, CURLOPT_POSTFIELDS, json_encode($obj));
-	      curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1) ;
-	      curl_setopt($conn, CURLOPT_CUSTOMREQUEST, strtoupper("PUT"));
-	      curl_setopt($conn, CURLOPT_FORBID_REUSE , 0) ;
-	      curl_setopt($conn, CURLOPT_CONNECTTIMEOUT, 0);
-	      curl_setopt($conn, CURLOPT_TIMEOUT, 60);
-	      try
-		{
-		  $res = curl_exec($conn);
-		  if (!$res || !$res['ok'] || $res['ok'] == false)
-		    echo "Error at entry $i:\n $res \n";
-		}
-	      catch (Exception $e)
-		{
-		  echo "An error ocurred: $e\nRetrying...\n";
-		  sleep(5);
-		  $res = curl_exec($conn);
-		  if (!$res || !$res['ok'] || $res['ok'] == false)
-		    echo "Error at entry $i:\n $res \n";
-		}
-	      if ($i % 100000 === 0) {
-		$es->refresh();
-		echo "Refreshed at $i entries\n";
-	      }
-	    }
-	  $flag = 1;
-	  $es->refresh();
-	}
-	catch (Exception $ex)
-	  {
-	    "Something went wrong within MongoDB: $ex\nRetrying...\n";
-	    $flag = 0;
-	    $cursor = getCursor($collection);
-	  }
+while ($flag == 0) {
+  try {
+    foreach ($cursor as $obj) {
+      $i++;
+      $mongoid = $obj['_id'];
+      unset($obj['_id']);
+      curl_setopt($conn, CURLOPT_URL, $URL . $i);
+      curl_setopt($conn, CURLOPT_POSTFIELDS, json_encode($obj));
+      curl_setopt($conn, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($conn, CURLOPT_CUSTOMREQUEST, strtoupper("PUT"));
+      curl_setopt($conn, CURLOPT_FORBID_REUSE, 0);
+      curl_setopt($conn, CURLOPT_CONNECTTIMEOUT, 0);
+      curl_setopt($conn, CURLOPT_TIMEOUT, 60);
+      try {
+        $res = curl_exec($conn);
+        if (!$res || !$res['ok'] || $res['ok'] == false) {
+          echo "Error at entry $i:\n $res \n";
+        }
+      } catch (Exception $e) {
+        echo "An error ocurred: $e\nRetrying...\n";
+        sleep(5);
+        $res = curl_exec($conn);
+        if (!$res || !$res['ok'] || $res['ok'] == false) {
+          echo "Error at entry $i:\n $res \n";
+        }
       }
-    echo "$i entries proccessed.\n";
-    exit();
+      if ($i % 100000 === 0) {
+        echo "Processed $i entries\n";
+      }
+    }
+    $flag = 1;
+  } catch (Exception $ex) {
+    "Something went wrong within MongoDB: $ex\nRetrying...\n";
+    $flag = 0;
+    $cursor = getCursor($collection);
   }
-echo "Couldn\'t connect to the ElasticSearch database.\n";
+}
+echo "$i entries proccessed.\n";
+exit();
