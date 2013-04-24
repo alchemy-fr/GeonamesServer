c25148d4617896b16f01c6200852a43d0019327d
Nicolas Le Goff <legoff.n@gmail.com>
2013-04-24 19:46:21 +0200

    Fix #33 Add 'geonames' ES tokenizer

--- resources/scripts/indexing/createIndex.sh
+++ resources/scripts/indexing/createIndex.sh
@@ -1,1 +1,17 @@
-curl -s -X POST $1
+curl -s -X POST $1 -d '{
+    "settings": {
+        "index": {
+            "analysis": {
+                "analyzer": {
+                    "geonames": {
+                        "type": "custom",
+                        "tokenizer": "whitespace",
+                        "filter": [
+                            "lowercase", "standard"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}'
