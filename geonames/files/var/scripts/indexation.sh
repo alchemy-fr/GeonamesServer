6b9c657b663e43cecd2a0ed2b30c992e8b7b67d4
Alexandre BRACH <alexandre.brach@gmail.com>
2019-07-12 17:38:25 +0200

    Env vars to forbid the indexation

--- geonames/files/var/scripts/indexation.sh
+++ geonames/files/var/scripts/indexation.sh
@@ -2,13 +2,15 @@
 
 set -xe
 
-if [ ! -f "/state/mongo-import" ]; then
-    # Import in mongo database
-    /var/scripts//mongo-import.sh
-fi
-if [ ! -f "/state/elastic-index" ]; then
-    # Indexation in Elasticsearch
-    /var/scripts/elasticsearch-indexation.sh
+if [ $NEVER_INDEX == 0 ]; then
+    if [ ! -f "/state/mongo-import" ]; then
+        # Import in mongo database
+        /var/scripts//mongo-import.sh
+    fi
+    if [ ! -f "/state/elastic-index" ]; then
+        # Indexation in Elasticsearch
+        /var/scripts/elasticsearch-indexation.sh
+    fi
 fi
 
 node /var/GeonamesServer/server.js
